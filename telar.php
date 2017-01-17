d08eefe91083845eba8f7f85ee66c3cda7ac79dc
Esequiel Palacios <esequielp@gmail.com>
2017-01-17 15:49:34 -0400

    inicio del proyecto

---
+++ telar.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+  </style>
+</head>
+<body>
+
+<div class="container-fluid bg-3 text-center">    
+  <h3>Telar de la Prosperidad</h3><br>
+  <div class="row">
+    <div class="col-sm-12">
+    <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%;height:150px;color:#FADD7F" alt="Image">
+    </div>
+  </div>  
+  <div class="row">
+      <p>PRIMER NIVEL</p>    
+    <div class="col-sm-6"> 
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%;height:100px" alt="Image">
+    </div>
+    <div class="col-sm-6">
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%;height:100px" alt="Image">
+    </div>
+  </div>
+
+  <div class="row">
+     <p>SEGUNDO NIVEL</p>
+    <div class="col-sm-3">
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-3"> 
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-3"> 
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-3">
+      <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+  </div>
+  <div class="row">
+    <p>TERCER NIVEL</p>
+    <div class="col-sm-3">
+        <div class="col-sm-6"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive"></div>
+        <div class="col-sm-6"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive"></div>
+    </div>
+    <div class="col-sm-3">
+        <div class="col-sm-6"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive"></div>
+        <div class="col-sm-6"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive"></div>
+    </div>
+  	<div class="col-sm-3">
+        <div class="col-sm-6"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive"></div>
+        <div class="col-sm-6"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive"></div>
+    </div>
+    <div class="col-sm-3">
+        <div class="col-sm-6"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive"></div>
+        <div class="col-sm-6"><img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive"></div>
+    </div>
+   </div>  
+</div><br><br>
+
+</body>
+</html>
